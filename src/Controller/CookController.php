5b5b50a2028ae03ee0823a1cd1a0d0ebee6b6efa
Pierre Migne <pierre.migne@hotmail.fr>
2021-03-04 19:15:26 +0100

    Ajout cuisiniers et leurs recettes

--- src/Controller/CookController.php
+++ src/Controller/CookController.php
@@ -2,15 +2,54 @@
 
 namespace App\Controller;
 
+use App\Entity\Cook;
+use App\Entity\Recipe;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CookController extends AbstractController
 {
-    #[Route('/cuisiniers', name: 'cook')]
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager){
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/cuisiniers', name: 'cooks')]
     public function index(): Response
     {
-        return $this->render('cook/index.html.twig');
+        $cooks = $this->entityManager->getRepository(Cook::class)->findAll();
+        return $this->render('cook/index.html.twig', [
+            'cooks'=>$cooks
+        ]);
+    }
+
+    #[Route('/cuisiniers/{slug}', name: 'cook')]
+    public function show($slug): Response
+    {
+        $cook = $this->entityManager->getRepository(Cook::class)->findOneBy(['slug'=>$slug]);
+        if (!$cook) {
+            return $this->redirectToRoute('cooks');
+        }
+
+        return $this->render('cook/show.html.twig', [
+            'cook'=>$cook
+        ]);
+    }
+
+    #[Route('/cuisiniers/{slug}/recettes', name: 'cookRecipes')]
+    public function recipes($slug): Response
+    {
+        $cook = $this->entityManager->getRepository(Cook::class)->findOneBy(['slug'=>$slug,]);
+
+        if (!$cook) {
+            return $this->redirectToRoute('cooks');
+        }
+
+        return $this->render('cook/cookRecipes.html.twig', [
+            'cook'=>$cook,
+        ]);
     }
 }
--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -18,12 +18,27 @@ class RecipeController extends AbstractController
         $this->entityManager = $entityManager;
     }
 
-    #[Route('/recettes', name: 'recipe')]
+    #[Route('/recettes', name: 'recipes')]
     public function index(): Response
     {
-        $recipes = $this->entityManager->getRepository(Recipe::class)->findAll();
+        $recipes = $this->entityManager->getRepository(Recipe::class)->findBy(['isVisible'=>'1']);
         return $this->render('recipe/index.html.twig', [
             'recipes'=>$recipes
+        ]);
+    }
+
+    #[Route('/recettes/{slug}', name: 'recipe')]
+    public function show($slug): Response
+    {
+        $cooks = $this->entityManager->getRepository(Cook::class)->findAll();
+        $recipe = $this->entityManager->getRepository(Recipe::class)->findOneBy(['slug'=>$slug]);
+        if (!$recipe) {
+            return $this->redirectToRoute('recipes');
+        }
+
+        return $this->render('recipe/show.html.twig', [
+            'recipe'=>$recipe,
+            'cooks'=>$cooks
         ]);
     }
 }
